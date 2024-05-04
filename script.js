2e6f9d8aa7c68fc78ae04299b1deed884503932a
Anirudh Subbarao <asubbarao450@gmail.com>
2024-05-04 14:40:20 -0500

    added functionality for updating monthly salary count after removing employee

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ function handleSubmit(event) {
     let employee = []
     if (employee[0] != "" || employee[1] != "" || employee[2] != "" || employee[3] != "" || employee[4] != "") {
 
-        employee[0] = document.getElementById('firstName').value;
-        employee[1] = document.getElementById('lastName').value;
-        employee[2] = document.getElementById('ID').value;
-        employee[3] = document.getElementById('Title').value;
-        employee[4] = document.getElementById('Annual Salary').value;
+        employee.push(document.getElementById('firstName').value);
+        employee.push(document.getElementById('lastName').value);
+        employee.push(document.getElementById('ID').value);
+        employee.push(document.getElementById('Title').value);
+        employee.push(document.getElementById('Annual Salary').value);
+
 
         //make sure to use Number function to 
         totalEmployeeSalary += Number(employee[4]);
-        
+
     }
 
     let tableBody = document.getElementById('tableBody');
@@ -36,27 +37,58 @@ function handleSubmit(event) {
         <td>${employee[1]}</td>
         <td>${employee[2]}</td>
         <td>${employee[3]}</td>
-        <td>${employee[4]}</td>
+        <td id >${employee[4]}</td>
+        <td><button id="delete" onClick="deletex(event)">Delete</button></td>
         </tr>
         `;
 
     }
 
     //querySelector worked but the getelementbyclassname didn't
-    
+
 
     updateFooter();
     //update footer with totalsalry/12
     //call the update footer function 
 }
 
-function updateFooter(){
+/* displays good coding practice by having different method
+to update footer */
+function updateFooter() {
 
     //to be called upon removing or adding an employee 
     //posibily only when the annualsalary is not blank
     let newFooter = document.querySelector('footer');
-   
-    newFooter.innerHTML = `${totalEmployeeSalary/12}`;
 
+
+    //console.log(ansalcol);
+
+    newFooter.innerHTML = `${totalEmployeeSalary / 12}`;
+
+}
+
+
+function deletex(event) {
+
+    //method needs to take in the click of the 
+    //delete button and delete entire table row associated with it
+    //by calling parent element
+
+    //check if there are other ways to write this command outside of 
+    //parentElement is called a selctor, can also use prviousSibling
+    let toDelete = event.target.parentElement.parentElement;
+
+    //returns the annual salary of the employee that we were deleting
+    //returns the adjacent table <td>
+    let sAnnualsal = Number(event.target.parentElement.previousSibling.previousSibling.innerHTML);
+    
+
+
+    totalEmployeeSalary -= sAnnualsal;
+
+    toDelete.remove();
+
+
+    updateFooter()
 
 }
